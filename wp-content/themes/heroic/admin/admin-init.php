f86ff1b9db99627b7ed730ca88ec101a2eba763a
hanamizuki10 <mizuki.hana@imprich-create.site>
2019-05-20 15:34:49 +0900

    コミットする必要のないテーマを無視リストに追加＆リポジトリ上から削除

--- wp-content/themes/heroic/admin/admin-init.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(is_admin()){
-	require get_stylesheet_directory() . '/admin/inc/class-webriti-about-page.php';
-}
-
-require get_stylesheet_directory() . '/admin/inc/plugin-include-control.php';
-require get_stylesheet_directory() . '/admin/inc/include-companion.php';
-
-
